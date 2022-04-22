ee026ca6adae7713b60947a6b40049feb8d98a4a
Tanner <tannercdolby@gmail.com>
2022-04-22 13:07:55 -0700

    Adds /dist

--- dist/get-tag.js
+++ dist/get-tag.js
@@ -5,14 +5,13 @@ function element(tag, content = "", selfClosing = false, tagInfo = tag) {
 ;
 /**
  * Create an HTML tag string.
- * @param {string} `tag` HTML tag name.
- * @param {string} `textContent` Text content for the element.
- * @param {Attributes} `attributes` Object representing attribute key/value pairs.
- * @param {boolean} `selfClosing` Text content for the element. Default: true
- * @return {string} A string representing the constructed HTML element.
+ * @param {string} tag HTML tag name.
+ * @param {string} textContent Text content for the element.
+ * @param {Attributes} attributes Object representing attribute key/value pairs.
+ * @param {boolean} selfClosing Text content for the element. Default: false
  * @return {string} A string representing the constructed HTML element.
  */
-module.exports = (tag, textContent = "", attributes, isSelfClosing = false) => {
+module.exports = (tag, textContent, attributes, isSelfClosing = false) => {
     const selfClosingTags = ["area", "base", "br", "col", "embed", "hr", "img", "input", "link", "meta", "param", "source", "track", "wbr"];
     tag = tag.toLowerCase();
     const selfClosing = selfClosingTags.includes(tag) || isSelfClosing;
