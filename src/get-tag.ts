debf7b9fabb2e89cc2552588aa17132235e4c02f
Tanner <tannercdolby@gmail.com>
2022-04-21 20:14:23 -0700

    refactor function logic

---
+++ src/get-tag.ts
@@ -0,0 +1,28 @@
+type Attributes = {
+    [key: string]: string | boolean;
+};
+
+function element(tag: string, content: string="", selfClosing: boolean=false, tagInfo: string=tag) {
+    return `<${tagInfo}>${!selfClosing ? content : ""}${!selfClosing ? `</${tag}>` : ""}`;
+};
+
+/**
+ * Create an HTML tag string.
+ * @param {string} tag HTML tag name.
+ * @param {Attributes} attributes An object representing attribute key/value pairs.
+ * @param {string} textContent Text content for the element.
+ * @return {string} A string representing the constructed HTML element.
+ */
+module.exports = (tag: string, textContent: string="", attributes: Attributes | undefined, isSelfClosing: boolean = false): string => {
+    const selfClosingTags = ["area", "base", "br", "col", "embed", "hr", "img", "input", "link", "meta", "param", "source", "track", "wbr"];
+    tag = tag.toLowerCase();
+    const selfClosing = selfClosingTags.includes(tag) || isSelfClosing;
+    let fields = "";
+    if (attributes && typeof attributes == "object" && Object.keys(attributes).length > 0) {
+        for (const key in attributes) {
+            fields += `${key}="${attributes[key]}" `;
+        }
+        return element(tag, textContent, selfClosing, `${tag} ${fields.slice(0, fields.length - 1)}`);
+    }
+    return element(tag, textContent, selfClosing);
+}
