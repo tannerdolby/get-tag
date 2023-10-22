6b1da0f6d64ea0385abb44e0db2f65f4c08fb632
Tanner <tannercdolby@gmail.com>
2023-10-22 12:44:44 -0700

    chore: better test descriptions

--- tests/get-tag.test.js
+++ tests/get-tag.test.js
@@ -1,66 +1,66 @@
 const getTag = require('../dist/get-tag');
 
 describe('getTag module', () => {
-  it('create <title> without attributes and with a closing tag', () => {
+  it('should create <title> with text content and a closing tag', () => {
     expect(getTag('title', 'Test Title')).toBe(
       '<title>Test Title</title>'
     );
   });
 
-  it('create <a> with attributes and a closing tag', () => {
+  it('should create <a> with attributes and a closing tag', () => {
     expect(getTag('a', 'Bug Saves The World', { href: 'foo' })).toBe(
       '<a href="foo">Bug Saves The World</a>'
     );
   });
 
-  it('create <meta>', () => {
+  it('should create <meta>', () => {
     expect(getTag('meta', null, { name: 'author', content: 'foo' })).toBe(
       '<meta name="author" content="foo">'
     );
   });
 
-  it('create <link>', () => {
+  it('should create <link>', () => {
     expect(getTag('link', null, { rel: 'stylesheet', href: 'style.css' })).toBe(
       '<link rel="stylesheet" href="style.css">'
     );
   });
 
-  it('create <p>', () => {
+  it('should create <p> tag with text content and class attribute', () => {
     expect(getTag('p', 'foo bar', { class: 'prose' })).toBe(
       '<p class="prose">foo bar</p>'
     );
   });
 
-  it('create <img>', () => {
+  it('should create <img> tag', () => {
     expect(getTag('img', null, { src: 'foo.jpg', alt: 'a test image' })).toBe(
       '<img src="foo.jpg" alt="a test image">'
     );
   });
 
-  it('create <h1>', () => {
+  it('should create <h1> tag', () => {
     expect(getTag('h1', 'Some Heading')).toBe('<h1>Some Heading</h1>');
   })
 
-  it('create empty non self closing', () => {
+  it('should create empty non self closing tag', () => {
     expect(getTag('title')).toBe('<title></title>');
   });
 
-  it('create empty self closing', () => {
+  it('should create empty self closing tag', () => {
     expect(getTag('meta')).toBe('<meta>');
   });
 
-  it('create img element with closing slash', () => {
+  it('should create self closing tag with optional closing slash', () => {
     expect(getTag('img', null, null, true, true)).toBe('<img />');
   });
 
-  it('throws error when a tag name is not provided', () => {
+  it('should throw error when a tag name is not provided', () => {
     const getTagMock = () => getTag();
     expect(getTagMock).toThrow(
       'Invalid HTML tag name. The first parameter `tag` is required and must be a string'
     );
   });
 
-  it('throws error when provided an invalid tag parameter', () => {
+  it('should throw error when provided an invalid tag parameter', () => {
     let mocks = [
       getTagMock = () => getTag(123),
       getTagMockTwo = () => getTag(['foo', 'bar']),
